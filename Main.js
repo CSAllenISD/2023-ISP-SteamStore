81ee3b79430b02acc1b7c08fb472aa209313e263
egg-tree <matthew.roman@student.allenisd.org>
2023-02-15 22:44:30 -0600

    added outline of nav

---
+++ Main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var links = document.getElementById("links");
+    if (links.style.display === "none") {
+	links.style.display = "block";
+    } else {
+	links.style.display = "none";
+    }
+}
