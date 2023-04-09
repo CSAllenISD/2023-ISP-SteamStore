d9914041f913ee688293165347e1ec238730b918
egg-tree <matthew.roman@student.allenisd.org>
2023-04-08 22:23:22 -0500

    icons change and links display when clicked

--- Main.js
+++ Main.js
@@ -1,15 +1,22 @@
 function animateNav(eventTracker, changedElement, property) {
-    var openButton = document.querySelector(eventTracker);
-    var overlay = document.querySelector(changedElement);
+    const openButton = document.querySelector(eventTracker);
+    const overlay = document.querySelector(changedElement);
     
     openButton.addEventListener('click', changeNavPropSize(overlay, property));
 }
 function changeNavPropSize(element ,property) {    
-    if (element.style[property] == "0%") {
+    if (element.style[property] == '0%') {
 	element.style[property] = '100%';
     } else {
 	element.style[property] = '0%';
     }
 }
 
+function toggleIcon(containerDiv, className, oldIcon, newIcon, linksClass) {
+    var icon = containerDiv.querySelector(className);
+    var links = containerDiv.parentElement.querySelector(linksClass);
+    icon.classList.toggle(oldIcon);    
+    icon.classList.toggle(newIcon);
+    links.style.display = (links.style.display === "none") ? "block" : "none";
+}
 
