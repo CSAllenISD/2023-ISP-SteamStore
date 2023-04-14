148251c7df991f96ddd81e287eec3de090e51a6c
egg-tree <matthew.roman@student.allenisd.org>
2023-04-13 21:33:50 -0500

    minor changes to header/footer

--- Main.js
+++ Main.js
@@ -1,27 +1,26 @@
-function animateNav(eventTracker, changedElement, property) {
-    const openButton = document.querySelector(eventTracker);
-    const overlay = document.querySelector(changedElement);
-    
-    openButton.addEventListener('click', changeNavPropSize(overlay, property));
-}
-function changeNavPropSize(element ,property) {    
-    if (element.style[property] == '0%') {
-	element.style[property] = '100%';
+//these first two functions are used in the navigation, but might be used for other elements later
+function openNav() {
+    const x = document.querySelector('.overlay');
+    if (x.style.width != '100%') {
+	x.style.width = '100%';
     } else {
-	element.style[property] = '0%';
+	x.style.width = '0';
     }
+    
+}
+// changes an icon
+function toggleIcon(containerDiv, className, oldIcon, newIcon) {
+    var icon = containerDiv.querySelector(className);
+    icon.classList.toggle(oldIcon);    
+    icon.classList.toggle(newIcon);    
+    icon.style.transform = icon.classList.contains(newIcon) ? "rotate(180deg)" : "rotate(0deg)";
 }
 
-function toggleIcon(containerDiv, className, oldIcon, newIcon, linksClass) {
-    var icon = containerDiv.querySelector(className);
-    var links = containerDiv.parentElement.querySelector(linksClass);
-    icon.classList.toggle(oldIcon);    
-    icon.classList.toggle(newIcon);
-    links.style.display = (links.style.display === "none") ? "block" : "none";
-    if (icon.classList.contains(newIcon)) {
-	icon.style.transform = "rotate(360deg)";
-    } else {
-	icon.style.transform = "rotate(90deg)";
-    }
+// this function is used for links like the ones in the footer
+function displayLinks(linksClass) {
+    const links = document.querySelector(linksClass);
+    links.style.display = (links.style.display != "block") ? "block" : "none";
 }
+
+
 
